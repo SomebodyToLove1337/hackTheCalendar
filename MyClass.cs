7e6dddaeaa0b7d1108f58f39800feb1189a7d0e4
SomebodyToLove1337 <68236595+SomebodyToLove1337@users.noreply.github.com>
2023-03-07 21:16:26 +0100

    erste änderungen

---
+++ MyClass.cs
@@ -0,0 +1,12 @@
+internal class MyClass
+{
+    public MyClass()
+    {
+    }
+
+    internal bool PrüfeAufUrlaub(string v)
+    {
+
+        throw new NotImplementedException();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Azure.Identity;
 using Microsoft.Graph;
 
-var scopes = new[] { "Calendars.Read" };
+var scopes = new[] { ".default" };
 var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
 {
   ClientId = "535ea419-74af-4047-9407-cff30fbb9e3e"
@@ -11,16 +11,21 @@ var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredent
 
 var graphClient = new GraphServiceClient(tokenCredential, scopes);
 
-var MeineKlassenInstanz = new MyClass();
-bool test = MeineKlassenInstanz.PrüfeAufUrlaub("xyz");
+//var MeineKlassenInstanz = new MyClass();
+//bool test = MeineKlassenInstanz.PrüfeAufUrlaub("xyz");
 
 // Hole dir Daten von der GraphAPI
 var me = await graphClient.Me.Calendar.Events.GetAsync((requestConfiguration) =>
 {
+  //requestConfiguration.QueryParameters.Select = new string[] { "start/dateTime", "end/dateTime", "subject"};
 	requestConfiguration.QueryParameters.Filter = "startsWith(subject,'test2')";
   requestConfiguration.QueryParameters.Orderby = new string []{ "start/dateTime asc" };
+  
 });
 
 var subject = me.Value[0].Subject;
+var start = me.Value[0].Start.DateTime;
+var end = me.Value[0].End.DateTime;
 
-Console.WriteLine($"Hello {me?.AdditionalData}!" + " " + test + " " + subject );
+Console.WriteLine($"Hello {me?.AdditionalData}!" + " " + subject + " - " + start + " - " + end);
+
