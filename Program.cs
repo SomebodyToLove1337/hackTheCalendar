01601a18c8388eadb967eb6625d46e34abf99e22
SomebodyToLove1337 <68236595+SomebodyToLove1337@users.noreply.github.com>
2023-03-07 20:48:28 +0100

    first changes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Azure.Identity;
+using Microsoft.Graph;
+
+var scopes = new[] { "Calendars.Read" };
+var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+{
+  ClientId = "535ea419-74af-4047-9407-cff30fbb9e3e"
+};
+
+var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+var graphClient = new GraphServiceClient(tokenCredential, scopes);
+
+var MeineKlassenInstanz = new MyClass();
+bool test = MeineKlassenInstanz.PrüfeAufUrlaub("xyz");
+
+// Hole dir Daten von der GraphAPI
+var me = await graphClient.Me.Calendar.Events.GetAsync((requestConfiguration) =>
+{
+	requestConfiguration.QueryParameters.Filter = "startsWith(subject,'test2')";
+  requestConfiguration.QueryParameters.Orderby = new string []{ "start/dateTime asc" };
+});
+
+var subject = me.Value[0].Subject;
+
+Console.WriteLine($"Hello {me?.AdditionalData}!" + " " + test + " " + subject );
