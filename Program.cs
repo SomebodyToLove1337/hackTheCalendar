0f924879381e7b517a016eeeffd9b4fc50c21000
SomebodyToLove1337 <benedikt.schuchart@outlook.com>
2023-03-08 17:24:15 +0100

    Change to App Authentication

--- Program.cs
+++ Program.cs
@@ -1,26 +1,39 @@
+// The client credentials flow requires that you request the
+// /.default scope, and preconfigure your permissions on the
+// app registration in Azure. An administrator must grant consent
+// to those permissions beforehand.
 using Azure.Identity;
 using Microsoft.Graph;
 
-var scopes = new[] { ".default" };
-var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+// Multi-tenant apps can use "common",
+// single-tenant apps must use the tenant ID from the Azure portal
+var tenantId = "93e5635d-6391-453c-afb7-1776f501135d";
+
+// Values from app registration
+var clientId = "535ea419-74af-4047-9407-cff30fbb9e3e";
+var clientSecret = "zjp8Q~gwQAYuHOXdfbE~TKm4N2ePYq4r5hOjgcj1";
+
+// using Azure.Identity;
+var options = new TokenCredentialOptions
 {
-    ClientId = "535ea419-74af-4047-9407-cff30fbb9e3e"
+    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
 };
 
-var apointmentFilter = "startsWith(subject,'test2')";
+// https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+var clientSecretCredential = new ClientSecretCredential(
+    tenantId, clientId, clientSecret, options);
 
-var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
 
-var graphClient = new GraphServiceClient(tokenCredential, scopes);
-
-//var MeineKlassenInstanz = new MyClass();
-//bool test = MeineKlassenInstanz.PrüfeAufUrlaub("xyz");
+var apointmentSubject = "test2";
 
 // Hole dir Daten von der GraphAPI
-var o365CalRequest = await graphClient.Me.Calendar.Events.GetAsync((requestConfiguration) =>
+var o365CalRequest = await graphClient.Users["64a018d3-7aaa-45fa-a63b-3d6528cbfe09"].Events.GetAsync((requestConfiguration) =>
 {
     //requestConfiguration.QueryParameters.Select = new string[] { "start/dateTime", "end/dateTime", "subject"};
-    requestConfiguration.QueryParameters.Filter = apointmentFilter;
+    requestConfiguration.QueryParameters.Filter = $"startsWith(subject,'{apointmentSubject}')";
     requestConfiguration.QueryParameters.Orderby = new string[] { "start/dateTime asc" };
 
 });
@@ -30,7 +43,14 @@ try
     var start = o365CalRequest.Value[0].Start.DateTime;
     var end = o365CalRequest.Value[0].End.DateTime;
 
+var o365CalRequest2 = await graphClient.Users["64a018d3-7aaa-45fa-a63b-3d6528cbfe09"].GetAsync((requestConfiguration) =>
+ {
+    requestConfiguration.QueryParameters.Select = new string[] { "language", "timeZone"};
+    //requestConfiguration.QueryParameters.Filter = $"startsWith(subject,'{apointmentSubject}')";
+    //requestConfiguration.QueryParameters.Orderby = new string[] { "start/dateTime asc" };
 
+}); 
+Console.WriteLine(o365CalRequest2.MailboxSettings);
 
     Console.WriteLine(subject + " - " + start + " - " + end);
 
@@ -47,7 +67,8 @@ try
 catch (ArgumentOutOfRangeException ex)
 {
 
-    Console.WriteLine($"Did not find an Apointment with");
+    Console.WriteLine($"Did not find an Apointment with the subject: {apointmentSubject}");
     Console.WriteLine(ex);
+
 }
 
