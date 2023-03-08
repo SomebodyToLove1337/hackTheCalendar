497462dcc2db2d483b5de66bbaba34df275482ca
SomebodyToLove1337 <benedikt.schuchart@outlook.com>
2023-03-08 10:09:16 +0100

    vierter Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOption
   ClientId = "535ea419-74af-4047-9407-cff30fbb9e3e"
 };
 
+var apointmentFilter = "startsWith(subject,'test2')";
+
 var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
 
 var graphClient = new GraphServiceClient(tokenCredential, scopes);
@@ -18,13 +20,13 @@ var graphClient = new GraphServiceClient(tokenCredential, scopes);
 var o365CalRequest = await graphClient.Me.Calendar.Events.GetAsync((requestConfiguration) =>
 {
   //requestConfiguration.QueryParameters.Select = new string[] { "start/dateTime", "end/dateTime", "subject"};
-	requestConfiguration.QueryParameters.Filter = "startsWith(subject,'test2')";
+	requestConfiguration.QueryParameters.Filter = apointmentFilter;
   requestConfiguration.QueryParameters.Orderby = new string []{ "start/dateTime asc" };
   
 });
-if (o365CalRequest.Value[0].Subject is not null)
+try
 {
-  var subject = o365CalRequest.Value[0].Subject;
+    var subject = o365CalRequest.Value[0].Subject;
   var start = o365CalRequest.Value[0].Start.DateTime;
   var end = o365CalRequest.Value[0].End.DateTime;
 
@@ -42,3 +44,10 @@ if (parsedStartDate < parsedEndDate)
   Console.WriteLine("Start Datum kleiner als Enddatum");
 }
 }
+catch (ArgumentOutOfRangeException ex)
+{
+  
+                  Console.WriteLine($"Did not find an Apointment with");
+                Console.WriteLine(ex);
+}
+
