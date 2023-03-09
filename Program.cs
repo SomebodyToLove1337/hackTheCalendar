5ed4ada2bf0ef4f33d3fe8fc50dacc60a2bda9ef
SomebodyToLove1337 <benedikt.schuchart@outlook.com>
2023-03-09 16:33:11 +0100

    extend mail function

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 // to those permissions beforehand.
 using Azure.Identity;
 using Microsoft.Graph;
+using Microsoft.Graph.Models;
 
 var scopes = new[] { "https://graph.microsoft.com/.default" };
 
@@ -52,20 +53,26 @@ var o365CalRequest2 = await graphClient.Users["64a018d3-7aaa-45fa-a63b-3d6528cbf
 }); 
 var mailboxSettings = o365CalRequest2?.MailboxSettings;
 var outOfOfficeActive = mailboxSettings.AutomaticRepliesSetting.Status;
-var outOfOfficeStart = mailboxSettings.AutomaticRepliesSetting.ScheduledStartDateTime;
-var outOfOfficeEnd = mailboxSettings.AutomaticRepliesSetting.ScheduledEndDateTime;
+var outOfOfficeStart = mailboxSettings.AutomaticRepliesSetting.ScheduledStartDateTime.DateTime;
+var outOfOfficeEnd = mailboxSettings.AutomaticRepliesSetting.ScheduledEndDateTime.DateTime;
+
 Console.WriteLine(outOfOfficeActive + " - " + outOfOfficeStart + " - " + outOfOfficeEnd);
 
     Console.WriteLine(subject + " - " + start + " - " + end);
 
     var parsedStartDate = DateTime.Parse(start);
     var parsedEndDate = DateTime.Parse(end);
+    var parsedStartOoODate = DateTime.Parse(outOfOfficeStart);
+    var parsedEndOoODate = DateTime.Parse(outOfOfficeEnd);
     Console.WriteLine("Start Datum:" + parsedStartDate);
     Console.WriteLine("End Datum:" + parsedEndDate);
+    Console.WriteLine("Start Datum OoO:" + parsedStartOoODate);
+    Console.WriteLine("End Datum OoO:" + parsedEndOoODate);
 
-    if (parsedStartDate < parsedEndDate)
+
+    if (parsedEndOoODate <= parsedEndDate)
     {
-        Console.WriteLine("Start Datum kleiner als Enddatum");
+        Console.WriteLine("Aktuell ist noch eine OoO Message aktiv!");
     }
 }
 catch (ArgumentOutOfRangeException ex)
@@ -76,3 +83,61 @@ catch (ArgumentOutOfRangeException ex)
 
 }
 
+/* var MailboxSettingsDiv =>
+{
+    "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users('username')/mailboxSettings",
+    
+    "timeZone": "W. Europe Standard Time",
+    "delegateMeetingMessageDeliveryOptions": "sendToDelegateOnly",
+    "dateFormat": "dd.MM.yyyy",
+    "timeFormat": "HH:mm",
+    "userPurpose": "user",
+    "automaticRepliesSetting": {
+        "status": "disabled",
+        "externalAudience": "none",
+        "internalReplyMessage": "",
+        "externalReplyMessage": "",
+        "scheduledStartDateTime": {
+            "dateTime": "2022-08-27T08:00:00.0000000",
+            "timeZone": "UTC"
+        },
+        "scheduledEndDateTime": {
+            "dateTime": "2022-08-28T08:00:00.0000000",
+            "timeZone": "UTC"
+        }
+    }, */
+
+var requestBody = new Microsoft.Graph.Users.Item.SendMail.SendMailPostRequestBody
+{
+	Message = new Message
+	{
+		Subject = "Meet for lunch?",
+		Body = new ItemBody
+		{
+			ContentType = BodyType.Text,
+			Content = "The new cafeteria is open.",
+		},
+		ToRecipients = new List<Recipient>
+		{
+			new Recipient
+			{
+				EmailAddress = new EmailAddress
+				{
+					Address = "DiegoS@xby1p.onmicrosoft.com",
+				},
+			},
+		},
+		CcRecipients = new List<Recipient>
+		{
+			new Recipient
+			{
+				EmailAddress = new EmailAddress
+				{
+					Address = "AdminBS@xby1p.onmicrosoft.com", 
+				},
+			},
+		},
+	},
+	SaveToSentItems = true,
+};
+await graphClient.Users["AdminBS@xby1p.onmicrosoft.com"].SendMail.PostAsync(requestBody);
